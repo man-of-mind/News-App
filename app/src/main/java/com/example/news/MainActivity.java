9f88b73b0ede2c2b6986f08f4b1b2e23371c32e7
timothy <timothyabiodun300@gmail.com>
2020-11-22 13:35:47 +0100

    couldn't complete due to electricity problem

--- app/src/main/java/com/example/news/MainActivity.java
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -49,11 +49,11 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             return true;
         }
-        else if (id == R.id.cache) {
-            Intent intent = new Intent(this, FavoriteNews.class);
-            startActivity(intent);
-            return true;
-        }
+//        else if (id == R.id.cache) {
+//            Intent intent = new Intent(this, FavoriteNews.class);
+//            startActivity(intent);
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
